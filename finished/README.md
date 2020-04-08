33c83296fa01096f5fb9e78aa6a5fcb510b8788e
unknown <austin.akers5@gmail.com>
2020-04-07 22:45:48 -0500

    tutorial dapp

---
+++ finished/README.md
@@ -0,0 +1,12 @@
+# MetaMask Test Dapp
+
+This is a simple test dapp for use in MetaMask e2e tests.
+
+Currently hosted [here](https://metamask.github.io/test-dapp/).
+
+## NOTE: Requires Manual Deployment
+After merging or pushing to `master`, please `yarn deploy` in the project root
+directory if any of the following files have changed:
+
+- `contract.js`
+- `index.html`
---
+++ start/contract.js
@@ -0,0 +1,40 @@
+/*global ethereum, MetamaskOnboarding */
+
+/*
+The `piggybankContract` is compiled from:
+
+  pragma solidity ^0.4.0;
+  contract PiggyBank {
+
+      uint private balance;
+      address public owner;
+
+      function PiggyBank() public {
+          owner = msg.sender;
+          balance = 0;
+      }
+
+      function deposit() public payable returns (uint) {
+          balance += msg.value;
+          return balance;
+      }
+
+      function withdraw(uint withdrawAmount) public returns (uint remainingBal) {
+          require(msg.sender == owner);
+          balance -= withdrawAmount;
+
+          msg.sender.transfer(withdrawAmount);
+
+          return balance;
+      }
+  }
+*/
+
+const forwarderOrigin = 'http://localhost:9010'
+
+
+
+const initialize = () => {
+  //You will start here 
+}
+window.addEventListener('DOMContentLoaded', initialize)
